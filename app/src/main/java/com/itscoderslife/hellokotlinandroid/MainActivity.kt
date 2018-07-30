820717e9539ab11ddf0eb546b56ed6104633858d
Damodar Shenoy <itsdamslife@gmail.com>
2018-07-30 21:59:50 +0530

    Weigght on mars implemented

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.itscoderslife.hellokotlinandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        buttonId.setOnClickListener {
-            textView.text = "Welcome " + editText.text + "!!"
+        calculatebutton.setOnClickListener {
+
+            if(weightOnEarth.text.isEmpty()) {
+                Toast.makeText(applicationContext, "Enter a valid weight in number", Toast.LENGTH_LONG).show()
+            } else {
+                val weight: Double = weightOnEarth.text.toString().toDouble()
+                val marsWt = (calculateWeightOnMars(weight)).toString()
+                resultview.text = "Your weight on mars is " + marsWt
+            }
         }
+    }
+
+    private fun calculateWeightOnMars(weight: Double) : Double {
+        val marsGravity = 0.38
+        return weight * marsGravity
     }
 }
