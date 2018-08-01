6d0931744c619eb730fe8132aefd60c49d5bc6eb
Damodar Shenoy <itsdamslife@gmail.com>
2018-08-01 18:58:24 +0530

    Example of Checkbox

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
@@ -2,29 +2,25 @@ package com.itscoderslife.hellokotlinandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        calculatebutton.setOnClickListener {
-
-            if(weightOnEarth.text.isEmpty()) {
-                Toast.makeText(applicationContext, "Enter a valid weight in number", Toast.LENGTH_LONG).show()
-            } else {
-                val weight: Double = weightOnEarth.text.toString().toDouble()
-                val marsWt = (calculateWeightOnMars(weight)).toString()
-                resultview.text = "Your weight on mars is " + marsWt
-            }
-        }
     }
 
-    private fun calculateWeightOnMars(weight: Double) : Double {
-        val marsGravity = 0.38
-        return weight * marsGravity
+    fun onCheckBoxClicked(view: View) {
+        view as CheckBox
+        val isChecked: Boolean = view.isChecked
+
+        when(view.id) {
+            R.id.cbfruits -> if (isChecked) { Log.d("Fruits ", "Checked") } else { Log.d("Fruits ", "Un-Checked") }
+            R.id.cbveggies -> if (isChecked) { Log.d("Veggies ", "Checked") } else { Log.d("Veggies ", "Un-Checked") }
+            R.id.cbdrinks -> if (isChecked) { Log.d("Drinks ", "Checked") } else { Log.d("Drinks ", "Un-Checked") }
+        }
     }
 }
