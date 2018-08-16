051dcc3acf028c6abe51f678676ff13fb5e494ab
Damodar Shenoy <itsdamslife@gmail.com>
2018-08-16 08:57:56 +0530

    Animal bios with detailed activity

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
@@ -15,72 +16,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.collections.HashMap
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    val REQUEST_CODE = 1
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            lionImgView.id -> {
+                val intent = Intent(this, description::class.java)
+                intent.putExtra("animal", "lion")
+                startActivity(intent)
+            }
+            cheetahImgView.id -> {
+                val intent = Intent(this, description::class.java)
+                intent.putExtra("animal", "cheetah")
+                startActivity(intent)
+            }
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(applicationContext,"On create",Toast.LENGTH_SHORT).show()
 
-        var uname = findViewById<EditText>(R.id.username)
-        var pswd = findViewById<EditText>(R.id.password)
+        print("Animal bios!!")
 
-        enter.setOnClickListener {
-            uname.setTextKeepState("")
-            pswd.setTextKeepState("")
-            Toast.makeText(applicationContext,"clear tapped",Toast.LENGTH_SHORT).show()
-        }
+        var cheetah = cheetahImgView
+        var lion = lionImgView
 
-
-        login.setOnClickListener {
-            var intent = Intent(this, HomePageActivity::class.java)
-            intent.putExtra("user", uname.text)
-            startActivityForResult(intent, REQUEST_CODE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-
-            val returnData = data!!.extras.get("return value").toString()
-            textView.setTextKeepState("Hope you had a nice trip to second activity!!")
-            Toast.makeText(applicationContext,returnData,Toast.LENGTH_LONG).show()
-        }
-
+        lion.setOnClickListener(this)
+        cheetah.setOnClickListener(this)
 
     }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        Toast.makeText(applicationContext,"On Destroy",Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        Toast.makeText(applicationContext,"On start",Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        Toast.makeText(applicationContext,"On stop",Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        Toast.makeText(applicationContext,"On pause",Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        Toast.makeText(applicationContext,"On resume",Toast.LENGTH_SHORT).show()
-//    }
-//
-//    override fun onPostResume() {
-//        super.onPostResume()
-//        Toast.makeText(applicationContext,"On post resume",Toast.LENGTH_SHORT).show()
-//    }
+
 }
