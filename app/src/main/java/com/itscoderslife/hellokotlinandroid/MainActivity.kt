029809be2107916d9e734d9c0e60a99a9fb83f48
Damodar Shenoy <itsdamslife@gmail.com>
2018-08-19 22:08:25 +0530

    implementation of recycler view

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/MainActivity.kt
@@ -1,37 +1,39 @@
 package com.itscoderslife.hellokotlinandroid
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.PorterDuff
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.view.View
-import android.widget.*
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.itscoderslife.hellokotlinandroid.adapter.PersonListAdapter
+import com.itscoderslife.hellokotlinandroid.model.Person
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
+
+    private var personList: ArrayList<Person>? = null
+    private var personListAdapter: PersonListAdapter? = null
+    private var layoutManager: RecyclerView.LayoutManager?  = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // List view < Adapters < Data source < Array of any type
 
-        val dataSrc = arrayOf("rang","shash","vish","chitr","jag","sav","gur","sud","rav","rash","ram","rag","dam","dee","vas","nam")
+        personList = ArrayList<Person>()
+        layoutManager = LinearLayoutManager(this)
+        personListAdapter = PersonListAdapter(personList!!, this)
 
-        val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, dataSrc)
+        personlist.layoutManager = layoutManager
+        personlist.adapter = personListAdapter
 
-        listviewid.adapter = adapter
-
-        listviewid.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(this, listviewid.getItemAtPosition(i).toString(), Toast.LENGTH_LONG).show()
+        for (i in 0..2200) {
+            val person = Person()
+            person.name = "Coder " + i
+            person.age = i + 22
+            personList!!.add(person)
         }
 
+        personListAdapter?.notifyDataSetChanged()
     }
 
 }
---
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/model/Person.kt
@@ -0,0 +1,6 @@
+package com.itscoderslife.hellokotlinandroid.model
+
+class Person {
+    var name: String? = null
+    var age: Int? = null
+}
