3d78b08c6a991e138c0f445876dc9e6b869e8a54
Damodar Shenoy <itsdamslife@gmail.com>
2018-10-10 19:16:48 +0530

    Made pop up functional and reload activity without animation

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/ChoresListActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/ChoresListActivity.kt
@@ -1,19 +1,25 @@
 package com.itscoderslife.hellokotlinandroid.activity
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
 import com.itscoderslife.hellokotlinandroid.R
 import com.itscoderslife.hellokotlinandroid.data.ChoreDataHandler
 import com.itscoderslife.hellokotlinandroid.data.ChoreListAdapter
 import com.itscoderslife.hellokotlinandroid.model.Chore
 import kotlinx.android.synthetic.main.activity_chores_list.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.popup_add_chore.*
+import kotlinx.android.synthetic.main.popup_add_chore.view.*
 
 class ChoresListActivity : AppCompatActivity() {
 
@@ -31,6 +37,7 @@ class ChoresListActivity : AppCompatActivity() {
         choreDBHandler = ChoreDataHandler(this)
 
         list = choreDBHandler?.readChores()
+        list!!.reverse()
 
         choreListAdapter = ChoreListAdapter(list!!,this)
         layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
@@ -55,10 +62,48 @@ class ChoresListActivity : AppCompatActivity() {
     }
 
     fun showCreateChorePopup() {
+
         val view = layoutInflater.inflate(R.layout.popup_add_chore, null)
+
+        var choreName = view.pchoretitleid
+        var choreAssignedBy = view.passignedbyid
+        var choreAssignedTo = view.passignedtoid
+        var saveBtn = view.psaveId
+
+
         dialogBuilder = AlertDialog.Builder(this).setView(view)
         dialog = dialogBuilder?.create()
-        dialog?.show()
+        val show = dialog?.show()
+
+
+        saveBtn.setOnClickListener {
+            if ( !TextUtils.isEmpty(choreName.text.toString().trim()) &&
+                    !TextUtils.isEmpty(choreAssignedBy.text.toString()) &&
+                    !TextUtils.isEmpty(choreAssignedTo.text.toString())) {
+
+                var chore = Chore()
+                chore.choreTitle = choreName.text.toString().trim()
+                chore.assignedBy = choreAssignedBy.text.toString()
+                chore.assignedTo = choreAssignedTo.text.toString()
+
+                val result = choreDBHandler!!.createChore(chore)
+                Toast.makeText(applicationContext, "Chore saved successfully with Id: $result", Toast.LENGTH_LONG).show()
+
+                dialog?.dismiss()
+
+                val intent = intent
+                overridePendingTransition(0, 0)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(applicationContext, "Enter all details, pls…", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 
 }
