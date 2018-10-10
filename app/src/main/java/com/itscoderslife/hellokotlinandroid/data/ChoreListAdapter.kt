ecbd624cc65c6445b8e4ee372ffeb47e785cec70
Damodar Shenoy <itsdamslife@gmail.com>
2018-10-10 22:21:57 +0530

    Implemented delete chore

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/data/ChoreListAdapter.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/data/ChoreListAdapter.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.itscoderslife.hellokotlinandroid.R
 import com.itscoderslife.hellokotlinandroid.model.Chore
 
@@ -31,16 +33,42 @@ class ChoreListAdapter(private var list: ArrayList<Chore>, private var context: 
         var choreAssignedTo = itemView.findViewById(R.id.listChoreAssignedto) as TextView
         var choreAssignedBy = itemView.findViewById(R.id.listChoreAssignedby) as TextView
         var choreDate = itemView.findViewById(R.id.listChoreDate) as TextView
+        var deleteBtn = itemView.findViewById(R.id.deleteId) as Button
+        var editBtn = itemView.findViewById(R.id.editId) as Button
 
         fun bindViews(chore: Chore) {
             choreName.text = chore.choreTitle
             choreAssignedBy.text = chore.assignedBy
             choreAssignedTo.text = chore.assignedTo
             choreDate.text = chore.showHumanDate(chore.assignedTime!!)
+            deleteBtn.setOnClickListener(this)
+            editBtn.setOnClickListener(this)
         }
 
-        override fun onClick(p0: View?) {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onClick(view: View?) {
+
+            var pos = adapterPosition
+            var chore = list[pos]
+
+            when(view!!.id) {
+                deleteBtn.id -> {
+                    Toast.makeText(context, "Delete button clicked", Toast.LENGTH_SHORT).show()
+                    deleteChore(chore.choreId!!)
+                    list.remove(chore)
+                    notifyItemRemoved(pos)
+                }
+
+                editBtn.id -> {
+                    Toast.makeText(context, "Edit button clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+
+        fun deleteChore(id: Int) {
+            if(id == null) { return }
+            var db: ChoreDataHandler = ChoreDataHandler(context)
+            db.deleteChore(id)
+        }
+        
     }
 }
