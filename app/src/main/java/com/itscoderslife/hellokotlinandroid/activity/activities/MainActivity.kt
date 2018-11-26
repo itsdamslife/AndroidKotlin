c430cd0101c8d27c0ec2d90117689656eb3869ed
Damodar Shenoy <itsdamslife@gmail.com>
2018-11-26 23:06:55 +0530

    Integrated firebase into a fresh android app

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
@@ -3,11 +3,21 @@ package com.itscoderslife.hellokotlinandroid.activity.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.itscoderslife.hellokotlinandroid.R
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Write a message to the database
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hi from My Chat App!!")
     }
 }
