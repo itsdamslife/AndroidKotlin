990e8b5e66514848c9453b7acfd43daf93dd9a0a
Damodar Shenoy <itsdamslife@gmail.com>
2018-12-16 00:02:34 +0530

    Main screen layout done

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
@@ -3,10 +3,7 @@ package com.itscoderslife.hellokotlinandroid.activity.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.itscoderslife.hellokotlinandroid.R
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Write a message to the database
-        val database = FirebaseDatabase.getInstance()
-        val myRef = database.getReference("message")
-
-        myRef.setValue("Hi from My Chat App!!")
+        textView.text = "Welcome to the JUNGLE :D"
     }
 }
