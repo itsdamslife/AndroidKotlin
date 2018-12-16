ea1297d01d32b876131ca31d11d0d84946c2351e
Damodar Shenoy <itsdamslife@gmail.com>
2018-12-16 14:42:57 +0530

    connected create account and login screens with main screen

--- app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
+++ app/src/main/java/com/itscoderslife/hellokotlinandroid/activity/activities/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.itscoderslife.hellokotlinandroid.activity.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.itscoderslife.hellokotlinandroid.R
 import kotlinx.android.synthetic.main.activity_main.*
+//import com.google.firebase.database.DatabaseReference
+//import com.google.firebase.database.FirebaseDatabase
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text = "Welcome to the JUNGLE :D"
+        // Write a message to the database
+//        val database = FirebaseDatabase.getInstance()
+//        val myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello, World!")
+
+        createAccountButton.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
